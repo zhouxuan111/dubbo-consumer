1e0f2eb8dad06db7975f1667304752bc78e09f1a
zhouxuan19 <xuanzhou19@creditease.cn>
2020-07-10 16:54:34 +0800

    测试

--- src/main/java/com/consumer/DubboConsumerApplication.java
+++ src/main/java/com/consumer/DubboConsumerApplication.java
@@ -3,9 +3,11 @@ package com.consumer;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @EnableDubbo
 @SpringBootApplication
+@ImportResource(locations = "consumer.xml")
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/consumer/test/TestController.java
+++ src/main/java/com/consumer/test/TestController.java
@@ -3,8 +3,8 @@ package com.consumer.test;
 import java.util.List;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.server.model.User;
 import com.server.service.TestService;
+import com.server.service.XmlService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +18,24 @@ public class TestController {
     @Reference
     private TestService testService;
 
+    @Reference
+    private XmlService xmlService;
+
     @GetMapping("test")
     public void test() {
         List<String> list = testService.test();
         System.out.println(list);
     }
 
-    @GetMapping("testUser")
+    @GetMapping("testXml")
+    public void testXml() {
+
+        xmlService.test();
+    }
+
+   /* @GetMapping("testUser")
     public void testUser() {
         User user = testService.testUser();
         System.out.println(user);
-    }
+    }*/
 }
